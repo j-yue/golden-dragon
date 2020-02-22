5c3a3b8e2e761893fd8fbded764c6ed35be2da20
Jeanette Yue <yue.jeanette@gmail.com>
2020-02-21 19:30:45 -0800

    testing if font img works on iphone

--- loader.js
+++ loader.js
@@ -1,1 +1,122 @@
-document.onreadystatechange=()=>{let e=document.readyState;if("loading"==e);else if("interactive"==e);else if("complete"==e){var t=()=>{let e=document.querySelector(".needs-validation");e.addEventListener("submit",t=>{t.preventDefault(),e.checkValidity()&&document.querySelector(".message-sent").classList.remove("hide-message"),e.classList.add("was-validated")})};let e=/iPad|iPhone|iPod/.test(navigator.platform)||"MacIntel"===navigator.platform&&navigator.maxTouchPoints>1;e&&(document.querySelector("#home").style.background="#000 center url(images/placeholder.jpg)",document.querySelector(".about-img").src="images/placeholder-front.jpg");var o=e=>{let t=e?"replace-placeholder-ios":"replace-placeholder";document.querySelector("#home").classList.add(t)},r=e=>{let t=e?"jpg":"webp";document.querySelector(".about-img").src=`images/front.${t}`},n=()=>{[".location",".about",".contact",".footer"].map(e=>document.querySelector(e).classList.add("show-section"))};if(document.querySelector("#main-nav").addEventListener("click",()=>{n()}),"IntersectionObserver"in window){let a={root:null,threshold:.5};const c=document.querySelector(".menu");new IntersectionObserver(e=>{e.forEach(e=>{e.isIntersecting&&renderMenu()})},a).observe(c);const l=document.querySelector(".home");new IntersectionObserver(a=>{a.forEach(a=>{a.isIntersecting?o(e):(n(),document.querySelectorAll(".ol-viewport").length<1&&createMap(),r(e),t())})},a).observe(l)}else setTimeout(()=>{o(e)},2e3),setTimeout(()=>{n(),createMap(),renderMenu(e),r(e),t()},3e3)}};
+
+/**
+ * reduce main thread work with lazy load
+ * lazy load approach: 
+ * hide everything except main nav, home, and container for menu
+ * then load the rest of the page as user scrolls away from home
+ * ...except when clicking/using main nav - force load the rest of the page to be visible
+ */
+document.onreadystatechange = () => {
+
+    let state = document.readyState;
+
+    if (state == 'loading') {
+
+    }
+
+    else if (state == 'interactive') {
+
+    }
+
+    else if (state == 'complete') {
+
+        // event listener for contact form
+        var formListener = () => {
+            let form = document.querySelector('.needs-validation');
+
+            form.addEventListener('submit', (ev) => {
+                ev.preventDefault();
+                if (form.checkValidity()) {
+                    document.querySelector('.message-sent').classList.remove('hide-message');
+                }
+                form.classList.add('was-validated');
+            });
+        }
+
+        // webp images unsupported on ios, load jpg images instead
+        let isIOS = /iPad|iPhone|iPod/.test(navigator.platform)
+            || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+
+        if (isIOS) {
+            document.querySelector('#home').style.background = '#000 center url(images/placeholder.jpg)';
+            document.querySelector('.about-img').src = 'images/placeholder-front.jpg';
+        }
+
+        
+        var replaceBg = (isIOS) => {
+            let sys = isIOS ? 'replace-placeholder-ios' : 'replace-placeholder';
+            home.classList.add(sys);
+            // document.querySelector('#home').classList.add(sys);
+        }
+
+        var replaceFront = (isIOS) => {
+            let format = isIOS ? 'jpg' : 'webp';
+            document.querySelector('.about-img').src = `images/front.${format}`;
+        }
+
+        var showSec = () => {
+            ['.location', '.about', '.contact', '.footer'].map(sec => document.querySelector(sec).classList.add('show-section'));
+        }
+
+        //border case where user uses main nav when some sections not visible
+        document.querySelector('#main-nav').addEventListener('click', () => {
+            showSec();
+        });
+
+        if ("IntersectionObserver" in window) {
+
+            let view = {
+                root: null,
+                threshold: .5
+            };
+
+            const menu = document.querySelector('.menu');
+            let loadMenu = entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        renderMenu();
+                    }
+                });
+            }
+
+            const menuObs = new IntersectionObserver(loadMenu, view);
+            menuObs.observe(menu);
+
+            const home = document.querySelector('.home');
+            let checkHome = entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        replaceBg(isIOS);
+                    } else {
+                        showSec();
+                        if (document.querySelectorAll('.ol-viewport').length < 1) createMap();
+                        replaceFront(isIOS);
+                        formListener();
+                    }
+
+                })
+            }
+
+            const homeObs = new IntersectionObserver(checkHome, view);
+            homeObs.observe(home);
+
+        } else {
+
+            //in case intersection observer unsupported
+            setTimeout(() => {
+                replaceBg(isIOS);
+            }, 2000);
+
+            setTimeout(() => {
+                showSec();
+                // document.querySelector('.ol-viewport').remove();
+                createMap();
+                renderMenu(isIOS);
+                replaceFront(isIOS);
+                formListener();
+            }, 3000);
+
+        }
+
+    }
+}
